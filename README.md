71994d80ea3bf1c977115e7f2a78c423e45f64e1
W-alker <2578451916@qq.com>
2023-09-24 19:49:44 +0800

    发版

---
+++ README.md
@@ -0,0 +1,59 @@
+# LoopCarousel
+解决需要通过无限滚动动画展示一组图片或者其他列表的需求。
+
+图片或列表的每一项的宽度可以不固定。
+
+提供JS动画和CSS动画两种方式。
+
+## install
+```ps
+npm install loop-carousel -S
+```
+
+## import
+
+这个包只是一个方法，生产版本在`/dist`目录下:
+```js
+const loopCarousel = require('loop-carousel')
+/* or */
+import loopCarousel from 'loop-carousel/dist'
+```
+
+## use
+首先我们需要一个容器并设置了`overflow:hidden`，然后这个展示列表作为唯一子元素在其中，同时这个展示列表最好是个弹性盒，例如: 
+```html
+<div class="loop-carousel-wrapper" style="overflow:hidden;">
+    <div class="loop-carousel-list" style="display:flex;">
+        <img src=""><img src=""><img src=""><img src="">
+    </div>
+</div>
+```
+因为只需要简单的设置两个属性，所以就不额外写CSS文件了，展示列表的子项可以自行通过margin等方式设置间隔.    
+
+
+最基础的只需要这样使用,就可以看到动画了:    
+```js
+const el = document.querySelector('.loop-carousel-list')
+loopCarousel(el)
+```
+该方法会根据这个展示列表的子元素大小和视口大小自动拷贝适量的DOM元素追加至其后。
+
+其他配置参数:
+```js
+/**
+ * @param {HTMLDivElement} el 必须,一个块DOM
+ * @param {number} spaceBetween 如果通过margin或者其他方式设置了子元素之间的间隔,请传入这个间隔的px值,默认0
+ * @param {number} duration 每次滚动展示完成的时间,默认8s
+ * @param {boolean} useJs 是否使用JS动画,默认false
+ * @param {string} timingFn 动画函数,同CSSs的'animation-timing-function',使用JS动画的情况下无效只允许默认值'linear'
+ * @returns {object} 
+ */
+```
+
+使用该方法返回的对象提供了一些属性和两个方法`stopAnimation`和`runAnimation`用来控制停止和活动，这在一些情况下比较适用:
+```js
+const el = document.querySelector('.loop-carousel-list')
+const obj = loopCarousel(el)
+obj.stopAnimation() // 停止动画,使用JS动画会停在当前位置,而CSS动画则会重置到起始位置
+obj.runAnimation() // 继续执行动画
+```
---
+++ index.js
@@ -0,0 +1,85 @@
+export class LoopCarousel {
+    constructor(el, spaceBetween = 0, duration = 8, useJs = false, timingFn = 'linear') {
+        // 打标记
+        el.dataset.loopCarousel = 'true'
+        this.el = el
+        this.scrollWidth = el.scrollWidth
+        this.spaceBetween = spaceBetween || 0
+        this.duration = duration || 8
+        this.timingFn = timingFn || 'linear'
+
+        if (useJs) {
+            this.useJs = true
+            this.jsAni = null
+            this.runPx = 0
+            this.jsAniIsStop = false
+        }
+    }
+
+    appendNode() {
+        let screenWidth = document.documentElement.clientWidth
+
+        const childsW = Array.from(this.el.children).map(v => v.clientWidth)
+        for (let i = 0; i < childsW.length; i++) {
+            screenWidth -= childsW[i]
+            this.el.append(this.el.children[i].cloneNode())
+            if (screenWidth <= 0)
+                break
+        }
+    }
+
+    createJSAni() {
+        const secondMove = this.scrollWidth / this.duration / 180
+        this.jsAni = () => {
+            if (this.runPx >= this.scrollWidth) this.runPx = 0
+            else this.runPx += secondMove
+            this.el.style.transform = `translateX(-${this.runPx}px)`
+            if (!this.jsAniIsStop) window.requestAnimationFrame(this.jsAni)
+        }
+    }
+
+    createCSSAni() {
+        const moveLong = this.scrollWidth + this.spaceBetween / 2
+        const frames = `@keyframes loopCarousel_x_${this.scrollWidth} { to { transform: translateX(${-moveLong}px); } }`
+        const style = document.querySelector('style')[0] || document.createElement('style');
+        style.innerHTML += frames;
+        document.querySelector('style')[0] ? null : document.querySelector('head').appendChild(style);
+    }
+
+    createAnimation() {
+        this.useJs ? this.createJSAni() : this.createCSSAni()
+    }
+
+    runAnimation() {
+        if (this.useJs) {
+            this.jsAniIsStop = false
+            return this.jsAni()
+        }
+
+        let aniStr = `loopCarousel_x_${this.scrollWidth} ${this.duration}s infinite ${this.timingFn}`
+        this.el.style.animation = aniStr
+    }
+
+    stopAnimation() {
+        if (this.useJs) this.jsAniIsStop = true
+        else this.el.style.animation = ''
+    }
+}
+
+/**
+ * 无限滚动列表
+ * @param {HTMLDivElement} el 必须,一个块DOM
+ * @param {number} spaceBetween 如果通过margin设置了子元素之间的间隔,请传入这个间隔的px值,默认0
+ * @param {number} duration 每次滚动展示完成的时间,默认8s
+ * @param {boolean} useJs 是否使用JS动画,默认false
+ * @param {string} timingFn 动画函数,同CSSs的'animation-timing-function',使用JS动画的情况下无效只允许默认值'linear'
+ * @returns {object} 
+ */
+export function loopCarousel(el, spaceBetween = 0, duration = 8, useJs = false, timingFn = 'linear') {
+    if (el.dataset.loopCarousel) return
+    const obj = new LoopCarousel(el, spaceBetween, duration, useJs, timingFn)
+    obj.appendNode()
+    obj.createAnimation()
+    obj.runAnimation()
+    return obj
+}
